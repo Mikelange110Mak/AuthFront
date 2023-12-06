482b743ce98d14942801a55ce98a1657dc6140c6
Mak <infinitycorpkz@gmail.com>
2023-12-06 17:13:41 +0600

    add new btns, function deleteUser

--- public/front/js/admin.js
+++ public/front/js/admin.js
@@ -1,17 +1,31 @@
 const table = document.querySelector('table');
 
+
+
 const URL = 'http://localhost:5000/auth/'
 const token = localStorage.getItem('token')
 
-function drawTable(username, role) {
+async function drawTable(username, role) {
    const newRow = table.insertRow();
    const cell1 = newRow.insertCell(0);
    const cell2 = newRow.insertCell(1);
+   const cell3 = newRow.insertCell(2);
+
+   cell1.classList.add('users__username')
    cell2.classList.add('role')
+   cell3.classList.add('users__action-cnt')
 
    cell1.innerHTML = username;
    cell2.innerHTML = role;
 
+   if (!role.includes('ADMIN')) {
+      cell3.innerHTML = `
+      
+      <button class="users__delete-btn">Delete</button>
+
+      `
+   }
+   await deleteUser()
 }
 
 
@@ -31,3 +45,16 @@ const getUsers = async () => {
 
 }
 getUsers()
+
+function deleteUser() {
+
+   const deleteBtn = document.querySelectorAll('.users__delete-btn')
+   deleteBtn.forEach(btn => {
+      btn.addEventListener('click', function () {
+         let username = this.closest('tr').querySelector('.users__username').textContent;
+         console.log({ username });
+      })
+   })
+
+}
+
