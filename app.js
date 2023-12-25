a285e38e1a8e194d98236c66e8355e6d19182a5d
Mak <infinitycorpkz@gmail.com>
2023-12-25 19:10:49 +0600

    process to create the new form

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.use(express.static(__dirname + '/public'));
-
-//python -m http.server 8080
-app.listen(3000, () => {
-   console.log('Server is running on port 3000');
-});
