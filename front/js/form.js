398936c26b95eae8b5d12bdfef48f6978b5f8d5c
Mak <infinitycorpkz@gmail.com>
2024-01-04 18:42:14 +0600

    form rework

--- front/js/form.js
+++ front/js/form.js
@@ -19,38 +19,81 @@ const blockAuth = document.querySelector(".forms__auth"),
 
 let loginState = true
 const API_URL = 'http://localhost:5000/auth/'
+//---------------------------------------------------------------------------------------
 
 
-//  Функция смены формы с формы логина на регистрацию и наоборот
-function changeForm(formActive, formDisabled) {
-   formDisabled.classList.remove("active");
-   formActive.classList.add("active");
-
-   //  Визуал для того чтобы юзер точно понял, что форма поменялась
-   let formInsideCarousel = formActive.querySelector('.form')
-   formInsideCarousel.classList.add("backlight")
-   setTimeout(() => {
-      formInsideCarousel.classList.remove("backlight")
-   }, 400);
-
-}
+//  ОСНОВНОЙ ФУНКЦИОНАЛ:
 
 
 //  Назначение функции смены формы, на кнопки
 loginToRegisterBtn.addEventListener('click', () => {
    changeForm(blockRegister, blockAuth);
    loginState = false
-   console.log(loginState);
 });
 
 registerToLoginBtn.addEventListener('click', () => {
    changeForm(blockAuth, blockRegister);
    loginState = true
-   console.log(loginState);
 });
 
 
 
+//  Функция входа в аккаунт
+const login = async () => {
+
+   const data = fieldsData()
+
+   try {
+      const response = await axios.post(`${API_URL}login`, data)
+      const token = response.data.token
+
+      localStorage.setItem('token', token)
+      window.location.href = './front/users.html'
+
+
+
+   } catch (error) {
+      let errorStr = error.response.data.message
+      let errorArr = error.response.data[0]
+      console.log(errorStr || errorArr);
+      messageErrorFn(errorStr || errorArr)
+   }
+
+}
+
+
+//  Функция регистрации
+const registration = async () => {
+
+   const data = fieldsData()
+
+   try {
+      const response = await axios.post(`${API_URL}registration`, data)
+      console.log(response);
+   } catch (error) {
+      let errorStr = error.response.data.message
+      let errorArr = error.response.data[0]
+      messageErrorFn(errorStr || errorArr)
+   }
+
+}
+
+
+//  Вызов функций входа/регистрации на соответсвующие кнопки
+loginGoBtn.addEventListener('click', () => {
+   login()
+})
+registerGoBtn.addEventListener('click', () => {
+   registration()
+})
+
+//---------------------------------------------------------------------------------------
+
+
+
+// ВСПОМОГАТЕЛЬНЫЕ ФУНКЦИИ:
+
+//  Данные с инпутов
 const fieldsData = () => {
    let data
 
@@ -74,48 +117,47 @@ const fieldsData = () => {
 }
 
 
-const login = async () => {
+//  Сообщение об ошибке, если пользователь ввел хуйню
+const messageErrorFn = (errorMsg) => {
 
-   const data = fieldsData()
-
-   try {
-      const response = await axios.post(`${API_URL}login`, data)
-      const token = response.data.token
-
-      localStorage.setItem('token', token)
-      window.location.href = './front/users.html'
+   const headerMessage = document.querySelector(".header")
+   headerMessage.textContent = errorMsg
+   headerMessage.classList.remove('hide')
 
 
+   setTimeout(() => {
+      headerMessage.classList.add('hide')
+   }, 1800);
 
-   } catch (error) {
-      let errorStr = error.response.data.message
-      let errorArr = error.response.data[0]
-      console.log(errorStr || errorArr);
-   }
-
-}
-
-const registration = async () => {
-
-   const data = fieldsData()
-
-   try {
-      const response = await axios.post(`${API_URL}registration`, data)
-      console.log(response);
-   } catch (error) {
-      let errorStr = error.response.data.message
-      let errorArr = error.response.data[0]
-      console.log(errorStr || errorArr);
-   }
+   //  Визуал для юзера, подсветка формы
+   backlightFn('backlightError', 520)
 
 }
 
 
-loginGoBtn.addEventListener('click', () => {
-   login()
-})
-registerGoBtn.addEventListener('click', () => {
-   registration()
-})
+//  Функция смены формы с формы логина на регистрацию и наоборот
+function changeForm(formActive, formDisabled) {
+   formDisabled.classList.remove("active");
+   formActive.classList.add("active");
+
+   //  Визуал для юзера, подсветка формы
+   backlightFn('backlight', 400)
+}
 
 
+//  Функция подсветки формы, используется при свитче форм с логина на вход или подсветить мол ошибка
+function backlightFn(cssClass, time) {
+
+   const carouselInner = document.querySelector('.carousel-inner');
+   let activeDiv = carouselInner.querySelector('.active');
+   let formDiv = activeDiv.querySelector('.form');
+
+   formDiv.classList.add(`${cssClass}`)
+   setTimeout(() => {
+      formDiv.classList.remove(`${cssClass}`)
+   }, time);
+
+}
+
+
+//---------------------------------------------------------------------------------------
