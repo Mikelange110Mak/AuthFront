308d1b3622c42acd4596c106bec65def8b4a9d33
Mak <infinitycorpkz@gmail.com>
2024-01-10 17:49:47 +0600

    account page rework done

--- front/js/users.js
+++ front/js/users.js
@@ -1,33 +1,34 @@
-const usernameSpan = document.querySelector('.account__username')
-const userRoleSpan = document.querySelector('.account__user-roles')
-const logoutBtn = document.querySelector('.account__exit')
-
-const URL = 'http://localhost:5000/auth/'
+const API_URL = 'http://localhost:5000/auth/'
 const token = localStorage.getItem('token')
 
+const usernameBtn = document.querySelector('.navbar__account-btn'),
+   userRole = document.querySelector('.navbar__account-role'),
+   userLogoutBtn = document.querySelector('.navbar__account-exit')
 
+
+//  Функция указания юзернейма в личном кабинете, ну и ее роли соответственно
 function postUsername(username, roleArr) {
-   usernameSpan.textContent = username
+   usernameBtn.textContent = username
 
-   let rolesStr = ''
+   let rolesStr = 'Роль : '
    roleArr.forEach(e => {
       let editRole = e + ' '
       rolesStr += editRole
    });
-   userRoleSpan.textContent = rolesStr
+   userRole.textContent = rolesStr
 }
 
-
+//  Тут все просто, если логинится челик с ролью ADMIN, то его перебрасывает на страницу админ панели
 const getData = async () => {
 
-   const response = await axios.get(`${URL}content`,
+   const response = await axios.get(`${API_URL}content`,
       {
          headers:
             { Authorization: `Bearer ${token}` }
       }
    );
    let contentData = response.data.contentData
-
+   console.log(contentData);
    if (contentData.user.roles.includes("ADMIN")) window.location.href = './admin.html'
    else postUsername(contentData.user.username, contentData.user.roles)
 
@@ -36,10 +37,12 @@ const getData = async () => {
    console.log(localStorage);
 }
 getData()
+
+//  Выход с аккича, подтереть токен из локалстореджа
 const logOut = () => {
    window.location.href = 'http://localhost:8080/'
    localStorage.clear();
    console.log(localStorage);
 }
-logoutBtn.addEventListener('click', logOut)
+userLogoutBtn.addEventListener('click', logOut)
 
